79ee93e93278d3bdc29e31005bc1152f63986198
Thejellyfish1024 <alamin102410@gmail.com>
2024-03-22 19:59:54 +0600

    made the discussion section

--- src/app/components/Discussions.jsx
+++ src/app/components/Discussions.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+"use client"
+import React, { useEffect, useState } from 'react';
+import SingleDiscussion from './SingleDiscussion';
 
 const Discussions = () => {
+    const [allDiscussions, setAllDiscussions] = useState([]);
+
+    useEffect(() => {
+        fetch('/dummy.json')
+            .then(res => res?.json())
+            .then(data => {
+                // console.log(data);
+                setAllDiscussions(data)
+            })
+    }, [])
     return (
-        <div>
+        <div className='px-3'>
             <h2 className='text-2xl font-bold bg-slate-200 p-2 w-fit rounded-sm text-red-500'>Disscussions Fourm</h2>
+            <div className='flex flex-col gap-4 mt-8'>
+                {
+                    allDiscussions?.map(discussion =>
+                        <SingleDiscussion
+                            key={discussion?.id}
+                            discussion={discussion}
+                        ></SingleDiscussion>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/app/components/SingleDiscussion.jsx
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import React from 'react';
+import { FaRegHeart } from "react-icons/fa6";
+const SingleDiscussion = ({ discussion }) => {
+
+    return (
+        <div className=' pr-2 pb-2 bg-gray-300 rounded-lg shadow-xl'>
+            <div className='flex gap-3 border bg-white px-6 py-3 rounded-lg'>
+                <Image
+                    src={discussion?.image}
+                    width={500}
+                    height={500}
+                    className='w-10 h-10'
+                    alt='user'
+                ></Image>
+                <div className='space-y-2'>
+                    <div className='flex justify-between'>
+                        <p>
+                            <span className='text-lg font-semibold'>{discussion?.name}</span>
+                            <span className='bg-blue-800 text-white text-xs px-5 py-1 rounded-full ml-6'>Sector {discussion?.sector}</span>
+                        </p>
+                        <p className='text-blue-500 font-semibold text-sm'>2 min ago</p>
+                    </div>
+                    <p className='w-4/5'>{discussion?.description}</p>
+                    <div className='flex justify-between w-4/5'>
+                        <p className='flex font-semibold items-center gap-1'>
+                            <FaRegHeart className='text-xl'></FaRegHeart>
+                            <span>2k</span>
+                        </p>
+                        <p className='flex font-semibold items-center gap-1'>
+                            <FaRegHeart className='text-xl'></FaRegHeart>
+                            <span>2k</span>
+                        </p>
+                        <p className='flex font-semibold items-center gap-1'>
+                            <FaRegHeart className='text-xl'></FaRegHeart>
+                            <span>2k</span>
+                        </p>
+                        <p className='flex font-semibold items-center gap-1'>
+                            <FaRegHeart className='text-xl'></FaRegHeart>
+                            <span>2k</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SingleDiscussion;
